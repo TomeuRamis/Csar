559385a558955ac6d0e16ed39fc1667b453e5b4f
TomeuRamis <tomeuramis99@yahoo.es>
2020-02-02 15:46:44 +0100

    Veng va

---
+++ src/codigo/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author Charly Ponce
+ */
+public class Principal {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/Charly Ponce/Documents/NetBeansProjects/AnalizadorLexico/src/codigo/Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
---
+++ src/codigo/Tokens.java
@@ -0,0 +1,24 @@
+package codigo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Charly Ponce
+ */
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    ERROR
+}
