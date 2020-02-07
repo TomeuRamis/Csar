698069c3520a66a35a3d3ec9cd66a9e6fa222549
TomeuRamis <tomeuramis99@yahoo.es>
2020-02-07 22:49:15 +0100

    Mejorada la tabla de simbolos

--- src/codigo/TablaSimbolos.java
+++ src/codigo/TablaSimbolos.java
@@ -13,42 +13,43 @@ import java_cup.runtime.Symbol;
  * @author davidcan
  */
 public class TablaSimbolos {
-    
+
     private final int profMax = 16;
-    
+
     ArrayList<FilaTD> td;
     ArrayList<FilaTE> te;
     int[] ta;
     int n;
-    public enum Mvp{
+
+    public enum Mvp {
         dproc, dconst, dvar
     }
-    public enum Tipo{
+
+    public enum Tipo {
         tBool, tString, tInt, tNull
     }
-    
-    public TablaSimbolos(){
+
+    public TablaSimbolos() {
         td = new ArrayList<>();
         te = new ArrayList<>();
         ta = new int[profMax];
         n = 0;
     }
-    
-    
-    public void reset(){
+
+    public void reset() {
         n = 0;
         td.clear();
         te.clear();
         ta[n] = 0;
     }
-    
-    public FilaTD consulta(String id){
+
+    public FilaTD consulta(String id) {
         int i = 0;
-        
-        if(td.isEmpty()){
+
+        if (td.isEmpty()) {
             return null;
         }
-        while(i < td.size() && !id.equals(td.get(i).nombre)){
+        while (i < td.size() && !id.equals(td.get(i).nombre)) {
             i++;
         }
         /*if(i > td.size()-1){
@@ -56,74 +57,81 @@ public class TablaSimbolos {
         }else{
             return td.get(i);
         }*/
-        return (i > td.size()-1) ? null : td.get(i);
+        return (i > td.size() - 1) ? null : td.get(i);
     }
-    
-    public boolean add(String nombre,Tipo t, Mvp mvp){
+
+    public boolean add(String nombre, Tipo t, Mvp mvp) {
         FilaTD filatd = consulta(nombre);
-        if(filatd != null){
-            if(filatd.np != n){
+
+        if (filatd != null) {
+            int posAnterior;
+            if (filatd.np != n) {
                 ta[n]++;
-                FilaTE nuevaFilaTE = new FilaTE(filatd, td.indexOf(filatd));
+                posAnterior = td.indexOf(filatd);
+                FilaTE nuevaFilaTE = new FilaTE(filatd, posAnterior);
                 te.add(nuevaFilaTE);
                 td.remove(filatd);
-            } else{
+            } else {
                 return false;
             }
+            FilaTD nuevaFila = new FilaTD(nombre, t, n, mvp);
+            td.add(posAnterior, filatd);
+        } else {
+            FilaTD nuevaFila = new FilaTD(nombre, t, n, mvp);
+            td.add(filatd);
         }
-        FilaTD nuevaFila = new FilaTD(nombre, t, n,mvp);
-        td.add(nuevaFila);
         return true;
+
     }
-    
-    public boolean ponerParam(String idproc, String idparam, Tipo tipo){
+
+    public boolean ponerParam(String idproc, String idparam, Tipo tipo) {
         FilaTD fproc = consulta(idproc);
-        if(fproc.mvp != Mvp.dproc){
+        if (fproc.mvp != Mvp.dproc) {
             //Error
             System.out.println("ERROR: solo se pueden añadir paràmetros a subprogramas");
             return false;
         }
-       
+
         int i = fproc.first;
         int pp = -1;
-        while(i != -1 && !te.get(i).nombre.equals(idparam)){
+        while (i != -1 && !te.get(i).nombre.equals(idparam)) {
             pp = i;
             i = te.get(i).first;
         }
-        if(i != -1){
+        if (i != -1) {
             //ERROR
-            System.out.println("ERROR en ponerParam de "+idproc);
+            System.out.println("ERROR en ponerParam de " + idproc);
             return false;
         }
         int nou = ta[n];
-        ta[n] = ta[n+1];
+        ta[n] = ta[n + 1];
         te.get(nou).nombre = idparam;
         te.get(nou).np = -1;
         te.get(nou).tipo = tipo;
         te.get(nou).first = -1;
         te.get(nou).mvp = null;
-        if(pp == -1){
+        if (pp == -1) {
             fproc.first = nou;
-            
-        }else{
+
+        } else {
             te.get(pp).first = nou;
         }
         return true;
     }
-    
-    public void entraBloque(){
+
+    public void entraBloque() {
         n++;
-        ta[n] = ta[n-1];
+        ta[n] = ta[n - 1];
     }
-    
-    public void salBloque(){
-        int lini,lfin;
+
+    public void salBloque() {
+        int lini, lfin;
         lini = ta[n];
         n--;
         lfin = ta[n];
-        while(lini > lfin){
-            FilaTE filate = te.get(lini-1);
-            if(filate.np != -1){
+        while (lini > lfin) {
+            FilaTE filate = te.get(lini - 1);
+            if (filate.np != -1) {
                 String id = filate.nombre;
                 FilaTD filatd = consulta(id);
                 filatd.tipo = filate.tipo;
@@ -138,45 +146,48 @@ public class TablaSimbolos {
             lini--;
         }
     }
-    
-    public FilaTE getFilaTE(int index){
-        return (index>te.size()-1) ? null : te.get(index);
+
+    public FilaTE getFilaTE(int index) {
+        return (index > te.size() - 1) ? null : te.get(index);
     }
-    
-    public class FilaTD{
+
+    public class FilaTD {
+
         public String nombre;
         public Tipo tipo;
         //Object valor;
         public int np; //Ambito
         public int first;
         public Mvp mvp; //Indica si es una constante, una variable o un subprograma
-        
-        public FilaTD(String n, Tipo t, int amb, Mvp mvp){
+
+        public FilaTD(String n, Tipo t, int amb, Mvp mvp) {
             nombre = n;
             tipo = t;
             //valor = s.value;
             np = amb;
             this.mvp = mvp;
         }
-        public FilaTD(FilaTD f){
+
+        public FilaTD(FilaTD f) {
             nombre = f.nombre;
             tipo = f.tipo;
             //valor = f.valor;
             np = f.np;
             mvp = f.mvp;
         }
-        
+
     }
-    public class FilaTE extends FilaTD{
+
+    public class FilaTE extends FilaTD {
+
         int refTD;
+
         //Utiliza el campo first heredado de FilaTD como campo next
-        public FilaTE(FilaTD f, int ref){
+        public FilaTE(FilaTD f, int ref) {
             super(f);
             refTD = ref;
         }
-       
+
     }
-   
+
 }
-
-
